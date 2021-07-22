4ae11153d91732460422aef4acb471aaae481ee7
Omar Farooq Khan <ofarooq767@gmail.com>
2021-07-22 20:14:33 +0100

    Upload Analyser

---
+++ CodeAnalyzer/CodeAnalyzer/CodeAnalyzer.Test/LocalVaribleAnalyzerTest.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using VerifyCS = CodeAnalyzer.Test.CSharpAnalyzerVerifier<
+    CodeAnalyzer.LocalVaribleAnalyzer>;
+
+namespace CodeAnalyzer.Test
+{
+    [TestClass]
+    public class LocalVaribleAnalyzerTest
+    {
+
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var test = @"";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class test
+        {   
+            public void testMethod()
+            {
+                string {|#0:name|};
+            }
+        }
+    }";
+
+            DiagnosticResult expected = VerifyCS.Diagnostic("AddLocalPrefix").WithLocation(15, 24).WithArguments("name");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task TestMethod3()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class test
+        {   
+            public void testMethod()
+            {
+                string _name, {|#0:blame|};
+            }
+        }
+    }";
+
+            DiagnosticResult expected = VerifyCS.Diagnostic("AddLocalPrefix").WithLocation(15, 31).WithArguments("blame");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+    }
+}
---
+++ CodeAnalyzer/CodeAnalyzer/CodeAnalyzer/LocalVaribleAnalyzer.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Immutable;
+
+namespace CodeAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class LocalVaribleAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "AddLocalPrefix";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Naming";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.LocalDeclarationStatement); // Syntax Kind = What are we looking for to trigger this analyser?
+                                                                                                 // When we see a local declaration call this analyser.
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var _localDeclaration = (LocalDeclarationStatementSyntax)context.Node;
+            
+
+            foreach (VariableDeclaratorSyntax varibleDeclaratorNode in _localDeclaration.Declaration.Variables)
+            {
+
+                if (!varibleDeclaratorNode.Identifier.ValueText.StartsWith("_"))
+                {
+                    Diagnostic report = Diagnostic.Create(Rule, varibleDeclaratorNode.Identifier.GetLocation(), varibleDeclaratorNode.Identifier.ValueText);
+                    context.ReportDiagnostic(report);
+                }
+            }
+        }
+
+        private void AddTwoNumbers()
+        {
+            int NumberOne;
+            int NumberTwo;
+
+            NumberOne = 1; NumberTwo = 2;
+
+            Console.WriteLine(NumberOne + NumberTwo);
+        }
+    }
+}
